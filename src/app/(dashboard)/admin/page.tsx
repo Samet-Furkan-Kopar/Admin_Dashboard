66673766b3de1732cf64705023a353f89ff95bfd
Samet-Furkan-Kopar <sametkopar465@gmail.com>
2024-09-02 14:50:13 +0300

    feat: Add StudentPage component and update tailwind.config.ts
    - Added StudentPage component to src/app/(dashboard)/student/page.tsx
    - Updated tailwind.config.ts to include new color variables

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+            <div className="w-full lg:w-2/3">
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    <div className="w-full lg:w-1/3 h-[450px]"></div>
+                </div>
+                <div className="bottom"></div>
+            </div>
+            <div className="w-full lg:w-1/3">rights</div>
+        </div>
+    );
+};
 
-export default AdminPage
+export default AdminPage;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -118,13 +118,14 @@ const menuItems = [
 
   const Menu = () => {
     return (
-      <div>
+      <div className="mt-4 text-sm p-1">
         {menuItems.map((i)=> (
-          <div key={i.title}> 
-          <span>{i.title}</span>
+          <div className="flex flex-col gap-2" key={i.title}> 
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
           {i.items.map((item,index) => (
-            <Link key={index} href={item.href}>
+            <Link key={index} href={item.href} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
               <Image  src={`/icons${item.icon}`} alt="" width={20} height={20}/>
+              <span className="hidden lg:block">{item.label}</span>
             </Link>
           ))}
           </div>
