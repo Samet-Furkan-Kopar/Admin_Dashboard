346faf4fa54b0eb1e21589376bb9e3f4538706eb
Samet-Furkan-Kopar <sametkopar465@gmail.com>
2024-09-12 16:06:58 +0300

    feat: Add remotePatterns to next.config
    This commit adds the `remotePatterns` property to the `images` object in the `next.config` file. The `remotePatterns` array now includes a new hostname for remote images, specifically "images.pexels.com". This change allows the application to fetch images from the specified remote host.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({
             </Link>
             <Menu />
         </div>
-        <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll">
+        <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll flex flex-col">
             <Navbar />
             {children}
         </div>
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+import React from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const TeacherPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="p-4 flex flex-1 gap-4 flex-col xl:flex-row">
+            <div className="w-full xl:w-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Çalışma</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                {/* <EventCalender /> */}
+                <Announcements />
+            </div>
+        </div>
+    );
+};
 
-export default TeacherPage
+export default TeacherPage;
