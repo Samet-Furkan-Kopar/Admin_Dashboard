fc5c6a6162a880648b24a2fbf7cae82db4b56f60
Samet-Furkan-Kopar <sametkopar465@gmail.com>
2024-09-13 11:12:26 +0300

    refactor: Update parent list page and table component
    - Update the parent list page to display a list of parents instead of students
    - Update the table component to show parent-related data instead of student-related data
    - Add the "students" field to the Parent type
    - Remove the "studentId" and "grade" fields from the Parent type
    - Update the renderRow function in the ParentListPage component to handle the changes in the Parent type
    - Update the table columns in the ParentListPage component to reflect the changes in the Parent type
    - Update the table data in the ParentListPage component to use the parentsData instead of studentsData

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -1,38 +1,37 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { parentsData, role, studentsData,  } from "@/lib/data";
+import { classesData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Parent = {
+type Class = {
     id: number;
     name: string;
-    students: string[];
-    email?: string;
-    phone: string;
-    address: string;
+    capacity: number;
+    grade: number;
+    supervisor: string;
 };
 const columns = [
     {
-        header: "Info",
-        accessor: "info",
+        header: "Class Name",
+        accessor: "name",
     },
     {
-        header: "Student Names",
-        accessor: "students",
+        header: "Capacity",
+        accessor: "capacity",
         className: "hidden md:table-cell",
     },
     {
-        header: "Phone",
-        accessor: "phone",
-        className: "hidden lg:table-cell",
+        header: "Grade",
+        accessor: "grade",
+        className: "hidden md:table-cell",
     },
     {
-        header: "Address",
-        accessor: "address",
-        className: "hidden lg:table-cell",
+        header: "Supervisor",
+        accessor: "supervisor",
+        className: "hidden md:table-cell",
     },
     {
         header: "Actions",
@@ -40,24 +39,19 @@ const columns = [
     },
 ];
 
-const ParentListPage = () => {
-    const renderRow = (item: Parent) => (
+const ClassListPage = () => {
+    const renderRow = (item: Class) => (
         <tr
             key={item.id}
             className="border-b border-gray-200 even:bg-slate-100 text-sm hover:bg-colorPurpleLight"
         >
-            <td className="flex items-center gap-4 p-4">
-                <div className="flex flex-col">
-                    <h3 className="font-semibold">{item.name} </h3>
-                    <p className="text-xs text-gray-500">{item?.email}</p>
-                </div>
-            </td>
-            <td className="hidden md:table-cell">{item.students.join(",")}</td>
-            <td className="hidden md:table-cell">{item.phone}</td>
-            <td className="hidden md:table-cell">{item.address}</td>
+            <td className="flex items-center gap-4 p-4">{item.name}</td>
+            <td className="hidden md:table-cell">{item.capacity}</td>
+            <td className="hidden md:table-cell">{item.grade}</td>
+            <td className="hidden md:table-cell">{item.supervisor}</td>
             <td className="">
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
+                    <Link href={`/list/classes/${item.id}`}>
                         <button className="w-7 h-7 flex items-center justify-center rounded-full bg-colorSky">
                             <Image src="/icons/view.png" alt="" width={16} height={16} />
                         </button>
@@ -75,7 +69,7 @@ const ParentListPage = () => {
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             <div className="flex items-center justify-between">
-                <h1 className="hidden md:block text-lg font-semibold ">All Parents</h1>
+                <h1 className="hidden md:block text-lg font-semibold ">All Classes</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -85,17 +79,18 @@ const ParentListPage = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
                             <Image src="/icons/sort.png" alt="" width={14} height={14} />
                         </button>
-                        {role === "admin" && <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
-                            <Image src="/icons/plus.png" alt="" width={14} height={14} />
-                        </button>}
+                        {role === "admin" && (
+                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
+                                <Image src="/icons/plus.png" alt="" width={14} height={14} />
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
-
-            <Table columns={columns} renderRow={renderRow} data={parentsData} />
+            <Table columns={columns} renderRow={renderRow} data={classesData} />
             <Pagination />
         </div>
     );
 };
 
-export default ParentListPage;
+export default ClassListPage;
--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,38 +1,25 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { parentsData, role, studentsData,  } from "@/lib/data";
+import { role, studentsData, subjectsData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Parent = {
+type Subject = {
     id: number;
     name: string;
-    students: string[];
-    email?: string;
-    phone: string;
-    address: string;
+    teachers: string[];
 };
 const columns = [
     {
-        header: "Info",
-        accessor: "info",
+        header: "Subject Name",
+        accessor: "name",
     },
     {
-        header: "Student Names",
-        accessor: "students",
+        header: "Teachers",
+        accessor: "teachers",
         className: "hidden md:table-cell",
-    },
-    {
-        header: "Phone",
-        accessor: "phone",
-        className: "hidden lg:table-cell",
-    },
-    {
-        header: "Address",
-        accessor: "address",
-        className: "hidden lg:table-cell",
     },
     {
         header: "Actions",
@@ -40,21 +27,14 @@ const columns = [
     },
 ];
 
-const ParentListPage = () => {
-    const renderRow = (item: Parent) => (
+const SubjectsListPage = () => {
+    const renderRow = (item: Subject) => (
         <tr
             key={item.id}
             className="border-b border-gray-200 even:bg-slate-100 text-sm hover:bg-colorPurpleLight"
         >
-            <td className="flex items-center gap-4 p-4">
-                <div className="flex flex-col">
-                    <h3 className="font-semibold">{item.name} </h3>
-                    <p className="text-xs text-gray-500">{item?.email}</p>
-                </div>
-            </td>
-            <td className="hidden md:table-cell">{item.students.join(",")}</td>
-            <td className="hidden md:table-cell">{item.phone}</td>
-            <td className="hidden md:table-cell">{item.address}</td>
+            <td className="flex items-center gap-4 p-4">{item.name}</td>
+            <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
             <td className="">
                 <div className="flex items-center gap-2">
                     <Link href={`/list/teachers/${item.id}`}>
@@ -75,7 +55,7 @@ const ParentListPage = () => {
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             <div className="flex items-center justify-between">
-                <h1 className="hidden md:block text-lg font-semibold ">All Parents</h1>
+                <h1 className="hidden md:block text-lg font-semibold ">All Students</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -85,17 +65,19 @@ const ParentListPage = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
                             <Image src="/icons/sort.png" alt="" width={14} height={14} />
                         </button>
-                        {role === "admin" && <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
-                            <Image src="/icons/plus.png" alt="" width={14} height={14} />
-                        </button>}
+                        {role === "admin" && (
+                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-colorYellow ">
+                                <Image src="/icons/plus.png" alt="" width={14} height={14} />
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
 
-            <Table columns={columns} renderRow={renderRow} data={parentsData} />
+            <Table columns={columns} renderRow={renderRow} data={subjectsData} />
             <Pagination />
         </div>
     );
 };
 
-export default ParentListPage;
+export default SubjectsListPage;
