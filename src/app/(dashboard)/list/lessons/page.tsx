633bda43a09e158151c6a439a18d052d4b460428
Samet-Furkan-Kopar <sametkopar465@gmail.com>
2024-09-13 17:41:05 +0300

    refactor: Update icons in class and lesson list pages

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -45,7 +45,7 @@ const LessonListPage = () => {
                 <div className="flex items-center gap-2">
                     <Link href={`/list/classes/${item.id}`}>
                         <button className="w-7 h-7 flex items-center justify-center rounded-full bg-colorSky">
-                            <Image src="/icons/view.png" alt="" width={16} height={16} />
+                            <Image src="/icons/edit.png" alt="" width={16} height={16} />
                         </button>
                     </Link>
                     {role === "admin" && (
